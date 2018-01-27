dc24fb2cf7ae19a6966779dae70af0dd89154404
Hunachi <hanahanahunachi@gmail.com>
2018-01-27 23:52:13 +0900

    remake Base.

--- app/src/main/java/com/example/hunachi/githunaclient/view/MainActivity.kt
+++ app/src/main/java/com/example/hunachi/githunaclient/view/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.hunachi.githunaclient.view
 import android.os.Bundle
 import com.example.hunachi.githunaclient.R
 import com.example.hunachi.githunaclient.view.base.BaseActivity
+import com.example.hunachi.githunaclient.viewModel.MainViewModel
+import com.github.salomonbrys.kodein.android.appKodein
+import com.github.salomonbrys.kodein.instance
 
 class MainActivity : BaseActivity() {
+    
+    private val viewModel: MainViewModel by instance()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setViewModel(viewModel)
     }
 }
--- app/src/main/java/com/example/hunachi/githunaclient/view/base/BaseActivity.kt
+++ app/src/main/java/com/example/hunachi/githunaclient/view/base/BaseActivity.kt
@@ -1,32 +1,25 @@
 package com.example.hunachi.githunaclient.view.base
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.annotation.IdRes
-import android.support.annotation.LayoutRes
-import android.support.v7.app.AppCompatActivity
-import com.example.hunachi.githunaclient.MyApplication
 import com.example.hunachi.githunaclient.viewModel.base.BaseViewModel
-import com.github.salomonbrys.kodein.KodeinInjector
-import com.github.salomonbrys.kodein.android.AppCompatActivityInjector
-import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.android.KodeinAppCompatActivity
 
 /**
  * Created by hunachi on 2018/01/27.
  */
-abstract class BaseActivity: AppCompatActivity(), AppCompatActivityInjector{
-    
-    final override val injector: KodeinInjector = KodeinInjector()
+abstract class BaseActivity: KodeinAppCompatActivity(){
     
     private lateinit var viewModel: BaseViewModel
     
-    open fun setViewModel(viewModel: BaseViewModel, context: BaseActivity){
+    open fun setViewModel(viewModel: BaseViewModel){
         this.viewModel = viewModel
+        viewModel.create(this)
     }
     
+    @SuppressLint("MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeInjector()
     }
     
     override fun onStart() {
@@ -34,10 +27,9 @@ abstract class BaseActivity: AppCompatActivity(), AppCompatActivityInjector{
         viewModel.onStart()
     }
     
+    @SuppressLint("MissingSuperCall")
     override fun onDestroy() {
         super.onDestroy()
-        //TODO destroy other provider too.
         viewModel.onDestroy()
-        destroyInjector()
     }
 }
