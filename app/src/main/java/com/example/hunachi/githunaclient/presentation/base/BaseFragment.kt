8f64a986f9132d7d6c418d0a4de1b1367ca94d34
Hunachi <hanahanahunachi@gmail.com>
2018-01-28 00:04:03 +0900

    constitute packages again.

---
+++ app/src/main/java/com/example/hunachi/githunaclient/presentation/base/BaseFragment.kt
@@ -0,0 +1,9 @@
+package com.example.hunachi.githunaclient.presentation.base
+
+import com.github.salomonbrys.kodein.android.KodeinSupportFragment
+
+/**
+ * Created by hunachi on 2018/01/27.
+ */
+class BaseFragment: KodeinSupportFragment() {
+}
