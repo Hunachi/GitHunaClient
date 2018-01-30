81bab9717d6f2ecbb021b9b0caec92d739ba343a
Hunachi <hanahanahunachi@gmail.com>
2018-01-31 02:49:08 +0900

    remake Token(responseModel).

--- app/src/main/java/com/example/hunachi/githunaclient/data/api/responce/Token.kt
+++ app/src/main/java/com/example/hunachi/githunaclient/data/api/responce/Token.kt
@@ -1,6 +1,7 @@
 package com.example.hunachi.githunaclient.data.api.responce
 
 import android.support.annotation.RestrictTo
+import com.example.hunachi.githunaclient.util.Scopes
 import com.squareup.moshi.Json
 import se.ansman.kotshi.JsonSerializable
 
@@ -12,7 +13,7 @@ data class Token(
         //todo make kotshi.
         @Json(name = "access_token")
         val token: String,
-        val scope: String,
+        val scope: String?,
         @Json(name = "token_type")
         val tokenType: String
 )
