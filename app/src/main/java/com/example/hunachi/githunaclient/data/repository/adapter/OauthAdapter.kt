c2566b5d03d531931dddcd027709de6c8eddb495
Hunachi <hanahanahunachi@gmail.com>
2018-02-22 16:46:18 +0900

    fixed bug of scope.

--- app/src/main/java/com/example/hunachi/githunaclient/data/repository/adapter/OauthAdapter.kt
+++ app/src/main/java/com/example/hunachi/githunaclient/data/repository/adapter/OauthAdapter.kt
@@ -7,21 +7,20 @@ import com.example.hunachi.githunaclient.util.Key
 /**
  * Created by hunachi on 2018/01/29.
  */
-object OauthAdapter{
+object OauthAdapter {
     
-    /*if you want scope to change, please change item at mutableList.*/
-    private val scopes = mutableListOf("repo")
+    private const val scopes = "repo,gist,read:user"
     private const val baseUrl = "https://github.com/login/oauth/authorize"
     private const val clientId: String = Key.clientId
     private const val state: String = Key.state
     
-    private val url = baseUrl + "?" +
+    private const val url = baseUrl + "?" +
             "client_id=" + clientId +
-            "&scopes=" + scopes.mapIndexed { index, s -> if(index != scopes.lastIndex)s + "+" else s } +
+            "&scope=" + scopes +
             "&state=" + state
     
     val intent = Intent(
-            Intent.ACTION_VIEW,
-            Uri.parse(url)
+        Intent.ACTION_VIEW,
+        Uri.parse(url)
     )
 }
