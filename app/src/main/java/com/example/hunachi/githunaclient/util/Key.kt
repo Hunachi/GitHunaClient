1d71048031f14ecbb15a74bea5fd4276ed71122a
Hunachi <hanahanahunachi@gmail.com>
2018-03-19 09:49:04 +0900

    test

--- app/src/main/java/com/example/hunachi/githunaclient/util/Key.kt
+++ app/src/main/java/com/example/hunachi/githunaclient/util/Key.kt
@@ -1,14 +1,12 @@
 package com.example.hunachi.githunaclient.util
 
-import com.example.hunachi.githunaclient.BuildConfig
-
 /**
  * Created by hunachi on 2018/02/20.
  */
 class Key{
     companion object {
-        const val CLIENT_ID: String = BuildConfig.CLIENT_ID
+        const val CLIENT_ID: String = ""
         const val state: String = "git-huna-oauth"
-        const val CLIENT_SECRET: String = BuildConfig.CLIENT_SECRET
+        const val CLIENT_SECRET: String = ""
     }
 }
